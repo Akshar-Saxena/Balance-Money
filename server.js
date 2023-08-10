7a4543e0d7bdd1d09d57c395c0e412679871133d
Akshar Saxena <aksharsaxena2003@gmail.com>
2023-08-10 12:57:58 +0530

    final commit

--- server.js
+++ server.js
@@ -1,13 +1,5 @@
 const express = require('express')
 const path = require('path')
-// const mongoose = require('mongoose');
-// mongoose.connect('mongodb://127.0.0.1:27017/test')
-//     .then(() => {
-//         console.log('success')
-//     })
-//     .catch(() => {
-//         console.log('error')
-//     })
 
 const app = express()
 
@@ -27,6 +19,6 @@ app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, 'layouts/login.html'))
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Listening on port 3000')
 })
